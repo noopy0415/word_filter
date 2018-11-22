215195ba4c98b41a805883d4da70b2fe3b23fa01
noopy0415 <noopy0415@gmail.com>
2018-11-22 23:53:42 +0900

    C-4終了
    NGワードと変換する文字をセットにするために辞書にした

--- test_run.py
+++ test_run.py
@@ -1,11 +1,15 @@
 import word_filter
 
-my_filter = word_filter.WordFilter("アーセナル")
+my_filter = word_filter.WordFilter("アーセナル", "リバプル")
 
 print(my_filter.detect("昨日のアーセナルの試合アツかった!"))
 
 print(my_filter.detect("昨日のリバプールの試合アツかった!"))
 
-print(my_filter.censor("昨日のアーセナルの試合アツかった！"))
+# print(my_filter.censor("昨日のアーセナルの試合アツかった！"))
 
-print(my_filter.censor("昨日のリバプールの試合アツかった！"))
+# print(my_filter.censor("昨日のリバプールの試合アツかった！"))
+
+print(my_filter.replace("昨日のアーセナルの試合アツかった！"))
+
+print(my_filter.replace("昨日のリバプールの試合アツかった！"))
